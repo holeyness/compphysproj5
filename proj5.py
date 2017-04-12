7a033030eb8a1433e846b5ed1c5f98c615a41fb6
Ian Luo <ian@ianluo.com>
2017-04-12 16:32:18 -0400

    Lets freakin go

---
+++ proj5.py
@@ -0,0 +1,16 @@
+"""
+Tufts Comp Physics 2017
+Project 5: Time Dependent Schrodinger Equation
+By: Yueming Luo, Michael Rosen, Chase Conley
+
+Preface: ...?
+
+    
+"""
+
+
+def main():
+    pass
+
+if __name__ == 'main':
+    main()
