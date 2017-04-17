7413df8357ec48d697e4f89f7ff0d902d13ad34e
Ian Luo <ian@ianluo.com>
2017-04-17 14:00:04 -0400

    it kinda works

--- proj5.py
+++ proj5.py
@@ -22,8 +22,8 @@ dt = 80 / 1000
 x_domain = (-40, 40)
 
 x0 = 0
-k0 = 0
-sigma = 0.05
+k0 = 2.5
+sigma = 1
 
 
 def main():
@@ -35,7 +35,7 @@ def main():
     for (x, y), _ in np.ndenumerate(hamiltonian):
         if x == y:
             # Set diagnoal elements
-            hamiltonian[x, y] = 2 * alpha + v_step(y)
+            hamiltonian[x, y] = 2 * alpha + v_free(y)
         elif x < size and y - x == 1:
             # The element to the right of each diag element
             hamiltonian[x, y] = -1 * alpha
@@ -47,42 +47,50 @@ def main():
     hamiltonian[-1, 0] = -1 * alpha
     hamiltonian[0, -1] = -1 * alpha
 
-    print(hamiltonian)
 
     # Lets turn this imaginary, and add 1
     hamiltonian = 1j * dt * np.array(hamiltonian)
-    hamiltonian = 1 + np.array(hamiltonian)
+    for (x, y), val in np.ndenumerate(hamiltonian):
+        if val != 0:
+            hamiltonian[x, y] += 1
+    # hamiltonian = 1 + np.array(hamiltonian)
     hamiltonian = np.matrix(hamiltonian)
+    print(hamiltonian)
 
     # Take the inverse
     ham_inverse = inv(hamiltonian)
 
     # print('Hamiltonian', hamiltonian)
-    # print('Hamiltonian Inverse', ham_inverse)
+    print('Hamiltonian Inverse', ham_inverse)
 
     # Prepare a wavepacket, show the graph
     x = np.arange(x_domain[0], x_domain[1], dx)
     y = [wavepacket(i, x0, k0, sigma) for i in x]
     plt.plot(x, y)
+    plt.plot(x, np.real(y), color='b')
+    plt.plot(x, np.imag(y), color='g')
     plt.xlabel('X')
+    plt.ylim([-1.5, 1.55])
     plt.ylabel('Psi')
     plt.title('Wavepacket')
     plt.show()
 
     # Dot multiplication of the hamiltonian matrix, for 100 iterations
     psi_result = []
-    psi_multiplier = y
+    psi_multiplier = np.matrix(y).transpose()
+
 
     for _ in range(100):
-        psi_iteration = np.dot(ham_inverse, psi_multiplier).tolist()[0]
-        psi_result.append(psi_iteration)
+        print(ham_inverse.shape)
+        print(psi_multiplier.shape)
+        psi_iteration = np.dot(ham_inverse, psi_multiplier)
         psi_multiplier = psi_iteration
 
-        plt.plot(x, (np.absolute(psi_iteration) ** 2))
-        plt.plot(x, np.real(psi_iteration))
-        plt.plot(x, np.imag(psi_iteration))
-        plt.xlim([-300, 300])
-        # plt.ylim([0, 10])
+        # plt.plot(x, (np.absolute(psi_iteration) ** 2), color='r')
+        plt.plot(x, np.real(psi_iteration), color='b')
+        plt.plot(x, np.imag(psi_iteration), color='g')
+        plt.xlim([-20, 20])
+        plt.ylim([-1.5, 1.55])
         plt.show()
 
 
