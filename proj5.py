4a0e3d42dd09b6bcc50fa155de9d91579a5a2e5b
Chase Conley <chaseeconley@gmail.com>
2017-04-19 16:23:33 -0400

    Fixed potential barriers

--- proj5.py
+++ proj5.py
@@ -15,6 +15,52 @@ from numpy.linalg import inv, solve
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
+def wavepacket(x, x0, k0, sigma):
+    return (e ** (0.25 * (x - x0) * (4j * k0 + (-1 * x + x0) * (sigma ** 2))) * (pi ** 0.5) * sigma) / (sqrt(sqrt(2) * (pi ** (3/2) * sigma)))
+
+
+def v_free(x):
+    """Empty potential function"""
+    return 0
+
+
+def v_step(x):
+    """Step"""
+    if x < 0:
+        return 0
+    else:
+        return 1
+
+
+def v_ho(x):
+    """Harmonic oscillator"""
+    return np.square(0.6*x)
+
+
+def v_barrier(x):
+    """Barrier potential"""
+    if x < 1 and x > -1:
+        return 1
+    else:
+        return 0
+
+
+def v_crystal(x):
+    """Crystal"""
+    if x > 0 and (x // 1) % 2 == 0.0:
+        return -1
+    else:
+        return 0
+
+def v_well(x):
+    if x > 1 or x < -1:
+        return 99999999999999999
+    else:
+        return 0
+        
+    
+#Choose a potential
+v = v_crystal
 
 # Set up our constants
 dx = 80.0 / 1000
@@ -24,32 +70,33 @@ x_domain = (-40, 40)
 
 
 # Wave packet configs
-x0 = -1.0
+x0 = -10
 k0 = 3.59
-sigma = 0.89
+sigma = 1
 
 # Barrier Config
 a = 10
 b = 0.105
-xv0 = 5
+xv0 = 3
 
 
 def main():
     """Our main execution function"""
+    x = np.arange(x_domain[0], x_domain[1], dx)
     size = int(abs(x_domain[1] - x_domain[0]) / dx)
     hamiltonian = np.matrix(np.full([size, size], 0.0))    # Construct our matrix
 
     # Add diagnoal elements and fill in nearby elements
-    for (x, y), _ in np.ndenumerate(hamiltonian):
-        if x == y:
+    for (x_ind, y_ind), _ in np.ndenumerate(hamiltonian):
+        if x_ind == y_ind:
             # Set diagnoal elements
-            hamiltonian[x, y] = 2 * alpha + v_barrier(y)
-        elif x < size and y - x == 1:
+            hamiltonian[x_ind, y_ind] = 2 * alpha + v(x[x_ind])
+        elif x_ind < size and y_ind - x_ind == 1:
             # The element to the right of each diag element
-            hamiltonian[x, y] = -1 * alpha
-        elif x > 0 and x - y == 1:
+            hamiltonian[x_ind, y_ind] = -1 * alpha
+        elif x_ind > 0 and x_ind - y_ind == 1:
             # To the left of the diag
-            hamiltonian[x, y] = -1 * alpha
+            hamiltonian[x_ind, y_ind] = -1 * alpha
 
     # Set top right and bottom left corners to negative alpha
     hamiltonian[-1, 0] = -1 * alpha
@@ -74,7 +121,7 @@ def main():
     print('Hamiltonian Inverse', ham_inverse)
 
     # Prepare a wavepacket, show the graph
-    x = np.arange(x_domain[0], x_domain[1], dx)
+    
     print(len(x))
     y = [wavepacket(i, x0, k0, sigma) for i in x]
 
@@ -88,18 +135,22 @@ def main():
         psi_multiplier = psi_iteration
         
     # Animation
+    
+    v_plot = np.vectorize(v, otypes=[np.float])
 
     fig = plt.figure()
     ax = plt.axes(xlim=(-5, 5), ylim=(-1, 1))
     real_line, = ax.plot([], [], 'b', lw=1)
     im_line, = ax.plot([],[], 'g', lw=1)
     prob_line, = ax.plot([],[], 'r', lw=1)
+    barrier_line, = ax.plot([],[], 'm', lw=1)
     
     def init():
         real_line.set_data([], [])
         im_line.set_data([],[])
         prob_line.set_data([],[])
-        return real_line, im_line, prob_line
+        barrier_line.set_data([],[])
+        return real_line, im_line, prob_line, barrier_line
 
     # Animation function
     def animate(i, *fargs):
@@ -108,10 +159,11 @@ def main():
         real_line.set_data(x_list, np.real(psi[i]))
         im_line.set_data(x_list, np.imag(psi[i]))
         prob_line.set_data(x_list, np.absolute(psi[i]))
+        barrier_line.set_data(x_list, v_plot(x_list))
         return real_line, im_line, prob_line
     
     # Call the animator
-    anim = animation.FuncAnimation(fig, animate, init_func=init, fargs=[x, psi_result], interval=500,
+    anim = animation.FuncAnimation(fig, animate, init_func=init, fargs=[x, psi_result], interval=100,
                                    frames=10)
    
     #Save the animation                               
@@ -151,54 +203,14 @@ def main():
     real_line, = ax.plot([], [], 'b', lw=1)
     im_line, = ax.plot([],[], 'g', lw=1)
     prob_line, = ax.plot([],[], 'r', lw=1)
+    barrier_line, = ax.plot([],[], 'm', lw=1)
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, fargs=[x, results], interval=100, frames=len(x))
+    anim = animation.FuncAnimation(fig, animate, init_func=init, fargs=[x, results], interval=100, frames=len(results))
 
     plt.show()
 
 
-def wavepacket(x, x0, k0, sigma):
-    return (e ** (0.25 * (x - x0) * (4j * k0 + (-1 * x + x0) * (sigma ** 2))) * (pi ** 0.5) * sigma) / (sqrt(sqrt(2) * (pi ** (3/2) * sigma)))
 
-
-def v_free(x):
-    """Empty potential function"""
-    return 0
-
-
-def v_step(x):
-    """Step"""
-    if x < 0:
-        return 0
-    else:
-        return 1
-
-
-def v_ho(x):
-    """Harmonic oscillator"""
-    return np.square(x)
-
-
-def v_barrier(x):
-    """Barrier potential"""
-    if (0.0 + xv0) < x:
-        return a
-    else:
-        return 0
-
-
-def v_crystal(x):
-    """Crystal"""
-    if x > 0 and (x // 1) % 2 == 0.0:
-        return -1
-    else:
-        return 0
-
-def v_well(x):
-    if x > 1 or x < -1:
-        return 99999999999999999
-    else:
-        return 0
 
 if __name__ == "__main__":
     main()
